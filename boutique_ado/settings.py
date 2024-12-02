bf61d3fa66467eaa89987f179b3ea6c166c54318
Mark Mcloughlin <mark25mky@gmail.com>
2024-12-02 23:21:52 +0000

    fixed base.html

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -65,14 +65,13 @@ TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [
-            os.path.join(BASE_DIR, 'templates'),
-            os.path.join(BASE_DIR, 'templates', 'allauth'),
+            os.path.join(BASE_DIR, 'home', 'templates'),  # This should point to home/templates
         ],
-        'APP_DIRS': True,
+        'APP_DIRS': True,  # This tells Django to look for templates inside app-specific folders as well
         'OPTIONS': {
             'context_processors': [
                 'django.template.context_processors.debug',
-                'django.template.context_processors.request', # required by allauth 
+                'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
             ],
